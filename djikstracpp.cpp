a6a0fcc430ffb6018d2a3ef929d11ef4217b14f0
Sircrab <rovalfer@gmail.com>
2016-09-14 20:08:24 -0300

    Added tests to Dijkstra's algorithm

--- djikstracpp.cpp
+++ djikstracpp.cpp
@@ -1,7 +1,8 @@
-#include <queue>
+﻿#include <queue>
 #include <vector>
 #include <functional>
 #include <stdio.h>
+#include<cassert>
 
 #define mp make_pair
 
@@ -9,8 +10,10 @@ using namespace std;
 
 typedef pair<int, int> ii;
 
+int inf = 1e9;
+
 vector<int>* djikstra(vector<vector<ii> >& vec, int start, int dest) {
-	int inf = 1e9;
+	//int inf = 1e9;
 	priority_queue<ii, vector<ii >, greater<ii> > pq;
 	int N = vec.size();
 	pq.push(mp(0,start));
@@ -33,9 +36,66 @@ vector<int>* djikstra(vector<vector<ii> >& vec, int start, int dest) {
 
 }
 
+void testOneNodeGraph() {
+	//Test case with 1 node, expected value: 0, since start node is the same as destination
+	vector<vector<ii> > AL(1, vector<ii>());
+	vector<int> res = *djikstra(AL, 0, 0);
+	assert(res.size() == 1);
+	assert(res[0] == 0);
+}
+
+void testNormalGraph() {
+	/*Test case with "normal" directed graph. 5 nodes and 6 edges. Path between node 0 and 3 has an expected minimum path of 8.
+	0
+	(3)/ \(5)
+	/   \
+	▼	   ▼
+	1-(1)-► 2
+	/ \
+	(4)/   \(6)
+	▼     ▼
+	3 ◄-(1)-4
+	*/
+	vector<vector<ii> > AL(5, vector<ii>());
+	AL[0].push_back(mp(1, 3));
+	AL[0].push_back(mp(2, 5));
+	AL[1].push_back(mp(2, 1));
+	AL[2].push_back(mp(3, 4));
+	AL[2].push_back(mp(4, 6));
+	AL[4].push_back(mp(3, 1));
+	vector<int> res = *djikstra(AL, 0, 3);
+	assert(res.size() == 5);
+	assert(res[0] == 0);
+	assert(res[3] == 8);
+}
+
+void testConnectedComponentGraph() {
+	/*Test case with 2 connected components, path between them shouldn't exist. Expected result of: inf value.
+	0				3
+	/ \			   / ▲
+	(3)/   \(2)     (2)/   \(1)
+	▼     ▼		 ▼     \
+	1◄-(2)-2		 4-(3)-►5
+	*/
+	vector<vector<ii> > AL(6, vector<ii>());
+	AL[0].push_back(mp(1, 3));
+	AL[0].push_back(mp(2, 2));
+	AL[2].push_back(mp(1, 2));
+	AL[3].push_back(mp(4, 2));
+	AL[4].push_back(mp(5, 3));
+	AL[5].push_back(mp(3, 1));
+	vector<int> res = *djikstra(AL, 0, 3);
+	assert(res.size() == 6);
+	assert(res[0] == 0);
+	assert(res[3] == inf);
+}
+
 
 
 int main() {
+	testOneNodeGraph();
+	testNormalGraph();
+	testConnectedComponentGraph();
 	int V, E;
 	scanf("%d %d", &V, &E);
 	vector<vector<ii> > AL(V, vector<ii>());
@@ -49,3 +109,4 @@ int main() {
 	printf("%d\n", (*djikstra(AL, start, dest))[dest]);
 
 }
+
