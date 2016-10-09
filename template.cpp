d34b31da75f7968581f4e0d75518730860248367
Stefano Gioia <stefanogch@gmail.com>
2016-10-09 09:21:40 -0300

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define REP(i, j, k) for (int (i)=(j); (i)<(k); ++(i))
+#define correct(x, y, n, m) (0 <= (x) && (x) < (n) && 0 <= (y) && (y) <= (m))
+#define all(a) (a).begin(), (a).end()
+#define sz(a) int((a).size())
+#define pb(a) push_back(a)
+#define mp(x, y) make_pair((x), (y))
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<vii> vvii;
+
+const int INF = int(1e9);
+const ll INF64 = ll(1e18);
+const double EPS = 1e-9, PI = 3.1415926535897932384626433832795;
+
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
