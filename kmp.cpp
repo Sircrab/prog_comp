ff04f004ce0309f448e0b8101fde21c0c1531b0b
Stefano Gioia <stefanogch@gmail.com>
2016-10-23 14:36:06 -0300

    Update kmp.cpp

--- kmp.cpp
+++ kmp.cpp
@@ -2,37 +2,40 @@
 
 using namespace std;
 
-int MAX_N = 1000;
-
-vector<int> pmt (char* searched_string, int m) {
-    vector<int> pmt;        // the reset table.
-    int i = 0, j = -1;      // i -> "substring" index, j -> "prefix" index.
-    pmt.push_back(-1);      // init val of -1 -> makes i advance and j stay.
-                            // the same when necessary (on i++; j++; instructions).
-    while (i < m) {
-        while (j >= 0 && searched_string[i] != searched_string[j]) {
-            j = pmt[j];     // reset j. Note that if j=0 => pmt[j] = -1. This
-                            // will break loop, and next instruction will make j=0.
+vector<int> pmt(const string& pattern) {
+    vector<int> pmt;
+    int i = 0, j = -1;
+    pmt.push_back(-1);
+    while (i < pattern.size()) {
+        while (j >= 0 && pattern[i] != pattern[j]) {
+            j = pmt[j];
         }
-        // if j = -1 or characters match
-        i++; j++;           // advance both pointers.
-        pmt.push_back(j);   // write corresponding val to reset table.
+        i++; j++;
+        pmt.push_back(j);
     }
-    return(pmt);
+    return pmt;
 }
 
-void kmp(char* long_string, char* searched_string, vector<int> pmt, int n, int m) {
-    int i = 0, j = 0;       // i -> long_string index, j -> searched_string index.
-
-    while (i < n) {
-        while (j >= 0 && long_string[i] != searched_string[j]) {
-            j = pmt[j];     // reset j.
+void kmp(
+  const string& word,
+  const string& pattern,
+  const vector<int>& pmt) {
+    int i = 0, j = 0;
+    while (i < word.size()) {
+        while (j >= 0 && word[i] != pattern[j]) {
+            j = pmt[j];
         }
-        // if j = -1 or characters match.
-        i++; j++;           // advance both pointers.
-        if (j == m) {   // match!
+        i++; j++;
+        if (j == pattern.size()) {
             printf("Match at index %d\n", i - j); // modify to suit problem.
-            j = pmt[j];     // reset j for next possible match.
+            j = pmt[j];
         }
     }
+}
+
+int main(int argc, char const *argv[])
+{
+  string a, b; cin >> a >> b;
+  kmp(a, b, pmt(b));
+  return 0;
 }
